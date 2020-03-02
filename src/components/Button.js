3985a8677656d317aaac49b00293039941edaafa
Tkachenko Dmitrii <tkachenko.dmitrii.vlad@gmail.com>
2020-03-02 23:05:10 +0100

    links account for query string

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { StyledButton } from '../styles/primitives';
 
 const Button = ({ text, link, ...other }) => {
+    const [ finalLink, setfinalLink ] = useState(link);
+
+    useEffect(() => {
+        if (typeof window !== "undefined") {
+            const query = window.location.search;
+            setfinalLink(`${finalLink}${query}`);
+        }
+    }, []);
+
     return (
         <StyledButton
             {...other}
-            href={link}
+            href={finalLink}
             target="_blank"
             rel="noopener"
         >
